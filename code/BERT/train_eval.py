270d6edfd788958b9e4f85b5c997c57f559b06de
Lv Jianwei <76138339+Jianwei-Lv@users.noreply.github.com>
2022-02-17 16:01:33 +0800

    Update train_eval.py

--- code/BERT/train_eval.py
+++ code/BERT/train_eval.py
@@ -40,7 +40,7 @@ def eval(model, iterator, fname):
 
     words_all, triggers_all, triggers_hat_all, arguments_all, arguments_hat_all = [], [], [], [], []
     with torch.no_grad():
-        # for i, batch in enumerate(iterator):
+
         for i, (test, labels) in enumerate(iterator):
             trigger_logits, trigger_hat_2d, triggers_y_2d, _, _, _, _, _ = model(test, labels)
 
@@ -50,74 +50,39 @@ def eval(model, iterator, fname):
             triggers_hat_all.extend(trigger_hat_2d.cpu().numpy().tolist())
             arguments_2d=test[-1]
             arguments_all.extend(arguments_2d)
-            # if len(argument_keys) > 0:
-            #     arguments_hat_all.extend(argument_hat_2d)
-            # else:
-            #     batch_size = len(arguments_2d)
-            #     argument_hat_2d = [{'events': {}} for _ in range(batch_size)]
-            #     arguments_hat_all.extend(argument_hat_2d)
+
 
     triggers_true, triggers_pred, arguments_true, arguments_pred = [], [], [], []
     with open('temp', 'w',encoding='utf-8') as fout:
         for i, (words, triggers, triggers_hat) in enumerate(zip(words_all, triggers_all, triggers_hat_all)):
             triggers_hat = triggers_hat[:len(words)]
             triggers_hat = [idx2trigger[hat] for hat in triggers_hat]
-
-            # [(ith sentence, t_start, t_end, t_type_str)]
-            # [idx2trigger[trigger] for trigger in trigger_hat_2d[i].tolist()]
+            
             triggers_true_=find_triggers(triggers[:len(words)])
             triggers_pred_ = find_triggers(triggers_hat)
             triggers_true.extend([(i, *item) for item in triggers_true_])
             triggers_pred.extend([(i, *item) for item in triggers_pred_])
 
-            # [(ith sentence, t_start, t_end, t_type_str, a_start, a_end, a_type_idx)]
-            # for trigger in arguments['events']:
-            #     t_start, t_end, t_type_str = trigger
-            #     for argument in arguments['events'][trigger]:
-            #         a_start, a_end, a_type_idx = argument
-            #         arguments_true.append(( t_type_str, a_start, a_end, a_type_idx))
-            #
-            # for trigger in arguments_hat['events']:
-            #     t_start, t_end, t_type_str = trigger
-            #     if t_start>=len(words) or t_end>=len(words):
-            #         continue
-            #     for argument in arguments_hat['events'][trigger]:
-            #         a_start, a_end, a_type_idx = argument
-            #         if a_start >= len(words) or a_end >= len(words):
-            #             continue
-            #         arguments_pred.append((t_type_str, a_start, a_end, a_type_idx))
-
             for w, t, t_h in zip(words, triggers, triggers_hat):
                 fout.write('{}\t{}\t{}\n'.format(w, t, t_h))
-            # fout.write('#arguments#{}\n'.format(arguments['events']))
-            # fout.write('#arguments_hat#{}\n'.format(arguments_hat['events']))
-            fout.write("\n")
 
-    # print(classification_report([idx2trigger[idx] for idx in y_true], [idx2trigger[idx] for idx in y_pred]))
+            fout.write("\n")
 
     print('[trigger classification]')
     trigger_p, trigger_r, trigger_f1 = calc_metric(triggers_true, triggers_pred)
     print('P={:.3f}\tR={:.3f}\tF1={:.3f}'.format(trigger_p, trigger_r, trigger_f1))
 
-    # print('[argument classification]')
-    # argument_p, argument_r, argument_f1 = calc_metric(arguments_true, arguments_pred)
-    # print('P={:.3f}\tR={:.3f}\tF1={:.3f}'.format(argument_p, argument_r, argument_f1))
     print('[trigger identification]')
     triggers_true = [(item[0], item[1], item[2]) for item in triggers_true]
     triggers_pred = [(item[0], item[1], item[2]) for item in triggers_pred]
     trigger_p_, trigger_r_, trigger_f1_ = calc_metric(triggers_true, triggers_pred)
     print('P={:.3f}\tR={:.3f}\tF1={:.3f}'.format(trigger_p_, trigger_r_, trigger_f1_))
 
-    # print('[argument identification]')
-    # arguments_true = [(item[0], item[1], item[2]) for item in arguments_true]
-    # arguments_pred = [(item[0], item[1], item[2]) for item in arguments_pred]
-    # argument_p_, argument_r_, argument_f1_ = calc_metric(arguments_true, arguments_pred)
-    # print('P={:.3f}\tR={:.3f}\tF1={:.3f}'.format(argument_p_, argument_r_, argument_f1_))
 
     metric = '[trigger classification]\tP={:.3f}\tR={:.3f}\tF1={:.3f}\n'.format(trigger_p, trigger_r, trigger_f1)
-    # metric += '[argument classification]\tP={:.3f}\tR={:.3f}\tF1={:.3f}\n'.format(argument_p, argument_r, argument_f1)
+   
     metric += '[trigger identification]\tP={:.3f}\tR={:.3f}\tF1={:.3f}\n'.format(trigger_p_, trigger_r_, trigger_f1_)
-    # metric += '[argument identification]\tP={:.3f}\tR={:.3f}\tF1={:.3f}\n'.format(argument_p_, argument_r_, argument_f1_)
+    
     final = fname + ".P%.2f_R%.2f_F%.2f" % (trigger_p, trigger_r, trigger_f1)
     with open(final, 'w',encoding='utf-8') as fout:
         result = open("temp", "r",encoding='utf-8').read()
@@ -150,15 +115,8 @@ def train(config, model, train_iter, dev_iter):
             model.zero_grad()
             trigger_logits, trigger_hat_2d, triggers_y_2d,_, _, _, _,_= model(trains,labels)
 
-
             trigger_logits = trigger_logits.view(-1, trigger_logits.shape[-1])
             trigger_loss = criterion(trigger_logits, triggers_y_2d.view(-1))
-            # if len(argument_keys)>0 :
-            #     argument_loss = criterion(argument_hidden_logits, arguments_y_1d)
-            #
-            #     loss = trigger_loss + argument_loss
-            # else:
-            #     loss=trigger_loss
 
             loss=trigger_loss
             nn.utils.clip_grad_norm_(model.parameters(), 1.0)
@@ -166,7 +124,7 @@ def train(config, model, train_iter, dev_iter):
             loss.backward()
 
             optimizer.step()
-            # if i % 100 == 0:  # monitoring
+
             print("step: {}, loss: {}".format(i, loss.item()))
 
 
