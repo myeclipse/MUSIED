4ab56eb058c1793f40388eb31d9b408a924a37c2
Lv Jianwei <76138339+Jianwei-Lv@users.noreply.github.com>
2022-02-17 15:55:32 +0800

    Add files via upload

---
+++ code/BERT/const.py
@@ -0,0 +1,77 @@
+
+
+#22类食品安全事件
+TRIGGERS = ['包装破损',
+            '售卖假货',
+            '骑手偷吃',
+            '身体不适',
+            '食品不熟',
+            '异物',
+            '销售三无产品',
+            '感官异常',
+            '使用回收食品',
+            '使用违规食品添加剂',
+            '农药/金属残留',
+            '化冻',
+            '变质/发霉',
+            '宣传与实物不符',
+            '临期/过期',
+            '餐品标识缺失或内容异常',
+            '餐凉',
+            '投诉商家经营环境',
+            '资质证照不合规',
+            '销售违禁品',
+            '包装材料不符合国际标准',
+            '质量问题']
+
+#33类事件
+ACE_TRIGGERS=['Transaction:Transfer-Money',
+              'Life:Die',
+              'Justice:Extradite',
+              'Justice:Pardon',
+              'Justice:Acquit',
+              'Transaction:Transfer-Ownership',
+              'Justice:Fine',
+              'Business:Declare-Bankruptcy',
+              'Contact:Meet',
+              'Justice:Release-Parole',
+              'Justice:Execute',
+              'Justice:Convict',
+              'Conflict:Attack',
+              'Movement:Transport',
+              'Justice:Sentence',
+              'Life:Be-Born',
+              'Life:Divorce',
+              'Justice:Trial-Hearing',
+              'Contact:Phone-Write',
+              'Business:Merge-Org',
+              'Life:Marry',
+              'Justice:Sue',
+              'Personnel:Start-Position',
+              'Conflict:Demonstrate',
+              'Justice:Appeal',
+              'Personnel:Nominate',
+              'Business:End-Org',
+              'Justice:Charge-Indict',
+              'Justice:Arrest-Jail',
+              'Life:Injure',
+              'Personnel:End-Position',
+              'Business:Start-Org',
+              'Personnel:Elect']
+
+
+#无实体
+# ENTITIES=[
+#     '人物',
+#     '机构',
+#     '国家',
+#     '时间',
+#     '地点',
+#     '数量',
+#     '装备'
+# ]
+
+#无论元角色
+# ARGUMENTS=['Arg-Place', 'Arg-Subject', 'Arg-Time', 'Arg-Object', 'Arg-Number']
+
+
---
+++ code/BERT/run.py
@@ -0,0 +1,68 @@
+# coding: UTF-8
+import warnings
+warnings.filterwarnings("ignore")
+import time
+import torch
+import numpy as np
+from train_eval import train,eval, init_network
+from importlib import import_module
+import argparse
+from utils import build_dataset, build_iterator, get_time_dif
+# from pytorch_pretrained_bert import BertTokenizer
+from transformers import BertTokenizer
+from model import Model
+
+
+class Config(object):
+
+    """配置参数"""
+    def __init__(self,dataset):
+        self.model_name = 'bert'
+        self.train_path = dataset + '/train_sentence.json'
+        #self.dev_path = dataset + '/dev_sentence.json'
+        self.test_path = dataset + '/test_sentence.json'
+        self.save_path = dataset +'/result/'
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')   # 设备
+
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_epochs = 50                                            # epoch数
+        self.batch_size =64                                           # mini-batch大小
+        self.pad_size = 128                                             # 每句话处理成的长度(短填长切)
+        self.learning_rate = 2e-5                                       # 学习率
+        self.bert_path = './bert_pretrain'
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
+        self.hidden_size = 768
+        self.rnn_hidden = 768
+        self.num_layers = 1
+        self.dropout = 0.1
+
+
+if __name__ == '__main__':
+    dataset = 'data'  # 数据集
+
+    config=Config(dataset)
+
+    # np.random.seed(11)
+    # torch.manual_seed(11)
+    # torch.cuda.manual_seed_all(11)
+    # torch.backends.cudnn.deterministic = True  # 保证每次结果一样
+
+    start_time = time.time()
+    print("Loading data...")
+    train_data = build_dataset(config.train_path,config)
+    dev_data = build_dataset(config.dev_path, config)
+    test_data = build_dataset(config.test_path, config)
+
+    train_iter = build_iterator(train_data, config)
+    dev_iter = build_iterator(dev_data, config)
+    test_iter = build_iterator(test_data, config)
+
+    time_dif = get_time_dif(start_time)
+    print("Time usage:", time_dif)
+
+    # train
+    model = Model(config).to(config.device)
+    #model=torch.load(config.save_path+'latest_model.pt')
+    #eval(model,test_iter,'online-judge')
+    train(config, model, train_iter, dev_iter)
